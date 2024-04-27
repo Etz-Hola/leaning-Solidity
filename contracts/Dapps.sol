b6aaf888346a1cdfdc66d05d8b1200c735bac52a
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2024-04-27 12:18:47 +0100

    first commit

---
+++ contracts/Dapps.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Dapps {
+
+    uint storeData = 20;
+    string firstName = " hola pow";
+    bool isActive = true;
+
+    function set() {
+        // statement block
+    }
+
+    function get() {
+        // statement block
+    }
+
+    function store() {
+        uint storage;
+
+        function set(uint x) view public {
+            storage = x
+        }
+
+        function get() public return(uint) {
+        return storage
+        }
+    }
+
+
+    function store*() {
+        string name;
+
+        function set(string ola) {
+            name = "ola"
+        }
+    }
+
+
+    
+
+
+
+}
